ffc4c6dd4831cf68ee46da6702d4d9aa26478696
Deepu Sharma <niikhiil.official@gmail.com>
2026-09-20 18:09:24 +0530

    feat: centralize common components

---
+++ src/components/common/index.ts
@@ -0,0 +1,7 @@
+export * from "./AppCard";
+export * from "./AppText";
+export * from "./EmptyState";
+export * from "./ExpenseRow";
+export * from "./PrimaryButton";
+export * from "./QuickAction";
+export * from "./SectionHeader";
